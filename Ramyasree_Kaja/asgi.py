e6b1a64f1165be75d397b327677ac265310e3b59
ramyasreekaja <ramyakaja274@gmail.com>
2023-12-11 02:41:15 -0800

    first commit

---
+++ Ramyasree_Kaja/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Ramyasree_Kaja project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ramyasree_Kaja.settings')
+
+application = get_asgi_application()
