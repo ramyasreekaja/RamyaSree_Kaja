dcaff9df551a9fbf18e80e192915a2be3714b01a
ramyasreekaja <ramyakaja274@gmail.com>
2023-12-13 16:48:08 -0800

    commit

--- linkup/urls.py
+++ linkup/urls.py
@@ -5,8 +5,10 @@ from . import views
 urlpatterns = [
     path('', views.linkup_list, name='linkup_list'),
     path('contact/', views.linkup_form, name='linkup_form'),
-     path('contact/create/', views.linkup_form, name='linkup_create'),
+    path('create/', views.linkup_create, name='linkup_create'),
+    #path('contact/create/', views.linkup_create, name='linkup_create'),
     path('contact/<int:id>/', views.linkup_detail, name='linkup_detail'),
-    path('contact/<int:id>/edit/', views.linkup_edit, name='linkup_update'),
-    path('contact/<int:id>/delete/', views.linkup_delete, name='linkup__confirm_delete'),
+    path('edit_contact/<int:id>/', views.linkup_update, name='linkup_update'),
+    #path('contact/<int:id>/', views.linkup_view, name='linkup_view'),
+    path('contact/<int:contact_id>/confirm_delete/', views.linkup_confirm_delete, name='linkup_confirm_delete'),
 ]
--- linkup/views.py
+++ linkup/views.py
@@ -41,24 +41,52 @@ def linkup_edit(request, id):
     return render(request, 'linkup/linkup_update.html', {'form': form})
 
 # View to delete a contact
-def linkup_delete(request, id):
-    contact = get_object_or_404(Contact, id=id)
+def linkup_confirm_delete(request, contact_id):
+    contact = get_object_or_404(Contact, pk=contact_id)
     if request.method == "POST":
         contact.delete()
         return redirect('linkup_list')
     return render(request, 'linkup/linkup_confirm_delete.html', {'contact': contact})
 
-    from django.shortcuts import render, redirect
-from .forms import ContactForm  # Ensure this is correctly imported
+    
 
-#view for creating a contact
-def linkup_form(request):
+# #view for creating a contact
+# def linkup_form(request):
+#     if request.method == 'POST':
+#         form = ContactForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('linkup_list')
+#     else:
+#         form = ContactForm()
+#     return render(request, 'linkup/linkup_create.html', {'form': form})
+
+
+def linkup_update(request, contact_id):
+    contact = get_object_or_404(Contact, pk=contact_id)
     if request.method == 'POST':
-        form = ContactForm(request.POST)
+        form = ContactForm(request.POST, instance=contact)
         if form.is_valid():
             form.save()
             return redirect('linkup_list')
     else:
-        form = ContactForm()
-    return render(request, 'linkup/linkup_create.html', {'form': form})
+        form = ContactForm(instance=contact)
+    return render(request, 'linkup/linkup_update.html', {'form': form, 'contact': contact})
 
+
+def linkup_create(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('linkup:linkup_list')  # Redirect to the contact list page
+    else:
+        form = ContactForm()
+    
+    context = {'form': form}
+    return render(request, 'linkup/linkup_create.html', context)
+
+def linkup_update(request, id):
+    contact = get_object_or_404(Contact, id=id)
+    # Rest of the view logic
+    return render(request, 'linkup/linkup_update.html', {'contact': contact})
